2ac20e8767ead14154ebf4c2ce49482bde7f2b9d
Stephanie Cancel <stephaniejs1995@gmail.com>
2018-01-12 20:47:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,4 +21,7 @@ function sayHiToGrandma(string) {
   else if(string === string.toLowerCase()) {
     return "I can't hear you!"
   }
+  else if(string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
